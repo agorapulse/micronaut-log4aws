df18a712e37c9d3de5182e9dac31ba518c5b66fe
Vladimir Orany <vladimir@orany.cz>
2020-01-15 10:30:57 +0100

    fixed violations

--- subprojects/micronaut-log4aws/src/test/groovy/com/agorapulse/micronaut/log4aws/LogErrorInterceptorSpec.groovy
+++ subprojects/micronaut-log4aws/src/test/groovy/com/agorapulse/micronaut/log4aws/LogErrorInterceptorSpec.groovy
@@ -1,29 +1,40 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020 Vladimir Orany.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.agorapulse.micronaut.log4aws
 
 import io.micronaut.context.ApplicationContext
 import org.junit.Rule
 import org.junit.contrib.java.lang.system.SystemOutRule
-import org.slf4j.LoggerFactory
 import spock.lang.AutoCleanup
 import spock.lang.Specification
-
 import javax.inject.Singleton
+
 class LogErrorInterceptorSpec extends Specification {
 
-    private static final String LOGGED_EVENT = 'ERROR ThrowsIllegalArgumentException:27 - Exception executing method'
+    private static final String LOGGED_EVENT = 'ERROR ThrowsIllegalArgumentTester:44 - Exception executing method'
 
     @Rule SystemOutRule systemOutRule = new SystemOutRule().enableLog()
 
     @AutoCleanup ApplicationContext context = ApplicationContext.run()
 
-    void setup() {
-        LoggerFactory
-
-    }
-
-    void 'test exception logged'() {
+    void 'exception is logged'() {
         given:
-            ThrowsIllegalArgumentException tester = context.getBean(ThrowsIllegalArgumentException)
+            ThrowsIllegalArgumentTester tester = context.getBean(ThrowsIllegalArgumentTester)
         when:
             tester.normal()
         then:
@@ -32,9 +43,9 @@ class LogErrorInterceptorSpec extends Specification {
             systemOutRule.log.contains(LOGGED_EVENT)
     }
 
-    void 'test exception muted'() {
+    void 'exception is muted'() {
         given:
-            ThrowsIllegalArgumentException tester = context.getBean(ThrowsIllegalArgumentException)
+            ThrowsIllegalArgumentTester tester = context.getBean(ThrowsIllegalArgumentTester)
         when:
             tester.muted()
         then:
@@ -43,9 +54,9 @@ class LogErrorInterceptorSpec extends Specification {
             systemOutRule.log.contains(LOGGED_EVENT)
     }
 
-    void 'test exception filtered'() {
+    void 'exception is filtered'() {
         given:
-            ThrowsIllegalArgumentException tester = context.getBean(ThrowsIllegalArgumentException)
+            ThrowsIllegalArgumentTester tester = context.getBean(ThrowsIllegalArgumentTester)
         when:
             tester.filtered()
         then:
@@ -54,9 +65,9 @@ class LogErrorInterceptorSpec extends Specification {
             !systemOutRule.log.contains(LOGGED_EVENT)
     }
 
-    void 'test exception filtered and muted match'() {
+    void 'exception is filtered and muted match'() {
         given:
-            ThrowsIllegalArgumentException tester = context.getBean(ThrowsIllegalArgumentException)
+            ThrowsIllegalArgumentTester tester = context.getBean(ThrowsIllegalArgumentTester)
         when:
             tester.filteredAndMutedMatch()
         then:
@@ -65,9 +76,9 @@ class LogErrorInterceptorSpec extends Specification {
             systemOutRule.log.contains(LOGGED_EVENT)
     }
 
-    void 'test exception filtered and muted mismatch'() {
+    void 'exception is filtered and muted mismatch'() {
         given:
-            ThrowsIllegalArgumentException tester = context.getBean(ThrowsIllegalArgumentException)
+            ThrowsIllegalArgumentTester tester = context.getBean(ThrowsIllegalArgumentTester)
         when:
             tester.filteredAndMutedMismatch()
         then:
@@ -79,7 +90,7 @@ class LogErrorInterceptorSpec extends Specification {
 }
 
 @Singleton
-class ThrowsIllegalArgumentException {
+class ThrowsIllegalArgumentTester {
 
     @LogError
     void normal() {
